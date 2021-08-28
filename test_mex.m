50944e901376ea6a79ee587e00f8db37d6286144
Ali Aghaeifar <ali.aghaeifar.mri@gmail.com>
2021-08-28 14:02:04 +0100

    example with selective excitation

--- test_mex.m
+++ test_mex.m
@@ -2,10 +2,6 @@
 clc
 mex bloch_sim_mex.cpp bloch_sim.cpp -R2018a
 
-%% with ppl
-clc
-mex COMPFLAGS='$COMPFLAGS /DUSE_PPL' bloch_sim_mex.cpp bloch_sim.cpp -R2018a
-
 %% with openblas
 clc
 mex  COMPFLAGS='$COMPFLAGS /DUSE_PPL /DEIGEN_USE_BLAS' -I'./openblas0.3.17/include/' ...
@@ -16,27 +12,19 @@ mex  COMPFLAGS='$COMPFLAGS /DUSE_PPL /DEIGEN_USE_BLAS' -I'./openblas0.3.17/inclu
 % include: C:/Program Files (x86)/Intel/oneAPI/mkl/2021.3.0/include/
 % lib: C:/Program Files (x86)/Intel/oneAPI/mkl/2021.3.0/lib/intel64/ & C:/Program Files (x86)/Intel/oneAPI/compiler/2021.3.0/windows/compiler/lib/intel64_win
 clc
-
 mex  COMPFLAGS='$COMPFLAGS /DEIGEN_USE_MKL_ALL /DUSE_PPL /openmp /DMKL_LP64' -I'C:/Program Files (x86)/Intel/oneAPI/mkl/2021.3.0/include/' ...
      -L'C:/Program Files (x86)/Intel/oneAPI/mkl/2021.3.0/lib/intel64/' -lmkl_intel_lp64 -lmkl_intel_thread -lmkl_core...
      -L'C:/Program Files (x86)/Intel/oneAPI/compiler/2021.3.0/windows/compiler/lib/intel64_win' -llibiomp5md...
-     bloch_sim_mex.cpp bloch_sim.cpp -R2018a
- 
- %% with GPU - needs to install updated graphic driver
- % download CUDA from https://developer.nvidia.com/cuda-downloads
- % include: C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v11.4\include
- % lib: C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v11.4\lib\x64
-clc
-cd('D:\OneDrive - University College London\Matlab\B1GUI\ipopt_lib\bloch_simulator')
-mex  COMPFLAGS='$COMPFLAGS /DUSE_PPL /DUSE_GPU' -I'C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v11.4\include' ...
-     -L'./gpu_matrix_mul/x64/Release/' -lgpu_matrix_mul -L'C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v11.4\lib\x64' -lcublas -lcudart_static ...
      bloch_sim_mex.cpp bloch_sim.cpp -R2018a
  
 %% compile with MinGW64 Compiler (C++) and openmp
 % see https://uk.mathworks.com/matlabcentral/answers/279171
 clc
 mex CXXFLAGS='$CXXFLAGS -std=c++11 -fno-math-errno -ffast-math -march=native -fopenmp' LDFLAGS=-fopenmp bloch_sim_mex.cpp bloch_sim.cpp -R2018a
-%%
+ %% with GPU - needs to install updated graphic driver
+ % download CUDA from https://developer.nvidia.com/cuda-downloads
+ % include: C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v11.4\include
+ % lib: C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v11.4\lib\x64
 clc
 cd('D:\OneDrive - University College London\Matlab\B1GUI\ipopt_lib\bloch_simulator')
 % NVCCFLAGS='$NVCCFLAGS
@@ -45,29 +33,66 @@ mexcuda -DUSE_GPU -L'C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v11.4\li
         -L'C:/Program Files (x86)/Intel/oneAPI/mkl/2021.3.0/lib/intel64/' -lmkl_intel_lp64 -lmkl_intel_thread -lmkl_core ...
         -L'C:/Program Files (x86)/Intel/oneAPI/compiler/2021.3.0/windows/compiler/lib/intel64_win' -llibiomp5md...
         bloch_sim_mex.cpp bloch_sim.cpp ./gpu_matrix_mul/gpu_matrix_mul.cu -R2018a
+    
+ %%
+ mexcuda -DUSE_GPU -L'C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v11.4\lib\x64' -lcublas -lcudart ...
+         bloch_sim_mex.cpp bloch_sim.cpp ./gpu_matrix_mul/gpu_matrix_mul.cu -R2018a
    
 %% test speed
+% Constants 
+gamma   = 267522187.44;
+gamma_hz= gamma/2/pi;
+FA      = 45 * pi/180;
+ntime   = 400; % number of samples
+rf_len  = 4e-3; % second
+td      = rf_len/ntime; %  dwell time 
+ncoil   = 8;
+voxel_sz= [3, 3, 3] * 1e-3;
+fov     = [-60 45; -30 70; -90 90] * 1e-3;
+rf_tbw  = 12;
+is_sinc = true;
+is_selective = true;
 
-gamma = 267522187.44;
-flipangle = 90 * pi/180;
-ntime = 400; % number of samples
-dur = 2; % second
-npos = 28003; % number of spatial positions
-ncoil = 8;
 
-b1 = complex(ones(ntime,ncoil) * flipangle/gamma/dur/ncoil) ;
-sens = complex(ones(ncoil,npos));
-tp = dur/ntime;
+% positions
+[prx, pry, prz] = ndgrid(fov(1,1):voxel_sz(1):fov(1,2), fov(2,1):voxel_sz(2):fov(2,2), fov(3,1):voxel_sz(3):fov(3,2));
+sz      = size(prz);
+pr      = transpose([prx(:), pry(:), prz(:)]);
+npos    = size(pr, 2); % number of spatial positions
 
-gr = zeros(ntime, 3);
+% off-resonance
 b0 = zeros(npos, 1);
-pr = rand(npos, 3);
-m0 = [zeros(2,npos); ones(1,npos)];
 
-tic
-result = bloch_sim_mex(b1, gr, tp, b0, pr, 10000, 10000, sens, m0);
-result';
-toc
+% sinc pulse
+if is_sinc
+    t = linspace(-rf_len/2, rf_len/2, ntime)'; % must be column
+    x = pi*t*rf_tbw/rf_len + eps; % didn't use 2pi -> we are interested in full BW not only the positive
+    snc = sin(x) ./ x; 
+    hamming_window = 0.53836 + 0.46164*cos(2*pi * linspace(-0.5,0.5,ntime));
+    rf = snc .* hamming_window;
+    rf = repmat(rf / sum(rf), [1 ncoil]); % normalize
+    b1 = complex(rf * FA/gamma/td/ncoil);
+else
+    b1 = complex(ones(ntime, ncoil) * FA/gamma/rf_len/ncoil) ;
+end
+sens = complex(ones(ncoil, npos));
+
+% gradients
+gr = zeros(3, ntime);
+if is_selective
+    BW = rf_tbw/rf_len; % [Hz]
+    gr(3, :) = 10*BW / voxel_sz(3) / gamma_hz;
+end
+
+
+m0 = [zeros(2, npos); ones(1, npos)];
+
+% run
+result = bloch_sim_mex(b1, gr, td, b0, pr, 10000, 10000, sens, m0);
+result_xy = reshape(result(1,:), sz) + 1j*reshape(result(2,:), sz);
+result_z = reshape(result(3,:), sz);
+
+vin(abs(result_xy))
 %%
 x= rand(ntime, 8) + 1j*rand(ntime, 8);
 y= rand(npos, 8) + 1j*rand(npos, 8);
