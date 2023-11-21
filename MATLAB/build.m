fd17dd38f4c5e408fd0ad8f94e2825123f27fd48
Ali Aghaeifar <ali.aghaeifar@tuebingen.mpg.de>
2023-11-21 19:05:38 +0100

    single floating point

--- MATLAB/build.m
+++ MATLAB/build.m
@@ -8,7 +8,7 @@ out_path    = fullfile(parent_dir, 'lib');
 out_path    = build_dir;
 
 if isunix
-    flags = ' CXXFLAGS="\$CXXFLAGS -std=c++17 -D__MEASURE_ELAPSED_TIME__"';
+    flags = ' CXXFLAGS="\$CXXFLAGS -std=c++17 -D__MEASURE_ELAPSED_TIME__ -D__SINGLE_PRECISION__"';
     ink_dir =  ''; % [' -I' '"' '/usr/include/mkl/' '"'];
     % libs = ' /usr/lib/x86_64-linux-gnu/libmkl_intel_lp64.a /usr/lib/x86_64-linux-gnu/libmkl_intel_thread.a /usr/lib/x86_64-linux-gnu/libmkl_core.a -liomp5 -lpthread -lm -ldl -ltbb';
     libs = ' -lm -ldl -ltbb';
@@ -18,7 +18,7 @@ elseif ispc
     lnk_dir1 = ''; % [' -L' '"' fullfile(getenv('MKLROOT'), 'lib', 'intel64') '"'];   
     lnk_dir2 = ''; %[' -L' '"' fullfile(getenv('ONEAPI_ROOT'), 'compiler', 'latest', 'windows', 'compiler', 'lib',  'intel64_win') '"']; 
     libs = ''; %' -lmkl_intel_lp64 -lmkl_intel_thread -lmkl_core -llibiomp5md';
-    flags = ' COMPFLAGS="$COMPFLAGS /std:c++17 /D__MEASURE_ELAPSED_TIME__"';
+    flags = ' COMPFLAGS="$COMPFLAGS /std:c++17 /D__MEASURE_ELAPSED_TIME__ /D__SINGLE_PRECISION__"';
     eval(['mex ' flags ink_dir lnk_dir1 lnk_dir2 libs src1 src2 ' -R2018a' ' -outdir ' out_path]); 
 else
     error('Platform not supported')
